c03db4592c0d68e9a4f297badd860f8684f7353f
Martin Wind <martin.wind@im-c.at>
2024-05-19 22:11:40 +0200

    remove unused

--- src/resources/statement/InverseFunctionalIdentifier.ts
+++
@@ -1,9 +0,0 @@
-// import { Account } from "./Account";
-
-// Mawi: v2 has to be exactly one
-// export interface InverseFunctionalIdentifier {
-//   mbox?: string;
-//   mbox_sha1sum?: string;
-//   account?: Account;
-//   openid?: string;
-// }
